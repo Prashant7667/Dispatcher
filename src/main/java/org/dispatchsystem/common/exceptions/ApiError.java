824d4f2018f5e743c0e1ce33265a4222c397b795
tars36268 <tars36268@gmail.com>
2026-03-07 17:41:54 +0530

    Global Exception Handling

---
+++ src/main/java/org/dispatchsystem/common/exceptions/ApiError.java
@@ -0,0 +1,15 @@
+package org.dispatchsystem.common.exceptions;
+import java.time.LocalDateTime;
+
+public class ApiError {
+    private int status;
+    private String error;
+    private String message;
+    private LocalDateTime timestamp;
+    public ApiError(int status, String error, String message){
+        this.status=status;
+        this.error=error;
+        this.message=message;
+        this.timestamp=LocalDateTime.now();
+    }
+}
